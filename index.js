f424eed51ba9d331e6e043528465872c64057d2b
rob <robertwigmore1@gmail.com>
2024-02-16 13:55:28 +0000

    clears  console

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const clearConsole = require("clear-any-console");
+
+// Clears the console.
+clearConsole();
+
+console.log(`Ahmad Awais  lol!
+
+open source developer
+
+Twitter: https://twitter.com/elonmusk
+`);
